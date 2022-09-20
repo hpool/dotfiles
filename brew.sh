8db222c359edee06de2c3a53bb69ebfa5120f696
hpool <hhpool@gmail.com>
2022-09-20 21:55:56 +0900

    brew installの選別

--- brew.sh
+++ brew.sh
@@ -8,48 +8,43 @@ brew upgrade
 
 brew install awscli
 brew install direnv
-brew install erlang
+# brew install erlang
 brew install fzf
 $(brew --prefix)/opt/fzf/install --key-bindings --completion --no-update-rc
 brew install git
-brew install gnu-prolog
+# brew install gnu-prolog
 brew install go
-brew install hg
-brew install io
+# brew install hg
+# brew install io
 brew install jq
-brew install leiningen
+# brew install leiningen
 brew install lv
-#brew install anyenv
+# brew install anyenv
 brew install readline
-brew install sbt
-brew install scala
+#brew install sbt
+# brew install scala
 brew install tmux
 brew install tree
 brew install wget
-brew install w3m
-brew install xz
+# brew install w3m
+# brew install xz
 brew install zsh
 brew install the_silver_searcher
 
-brew tap homebrew/binary
-brew install packer
+# brew tap homebrew/binary
+# brew install packer
 
-brew tap laurent22/massren
-brew install massren
+# brew tap laurent22/massren
+# brew install massren
 
 # インストール
-brew install --cask dash
-brew install --cask franz
-brew install --cask gimp
-brew install --cask google-chrome
-brew install --cask google-japanese-ime
-brew install --cask hyperswitch
-brew install --cask iterm2
-brew install --cask imageoptim
-brew install --cask karabiner-elements
-brew install --cask pycharm
-brew install --cask goland
-brew install --cask virtualbox
-brew install --cask vagrant
+# brew install dash
+# brew install gimp
+brew install google-chrome
+brew install google-japanese-ime
+brew install iterm2
+brew install imageoptim
+brew install karabiner-elements
+brew install goland
 
 brew cleanup
